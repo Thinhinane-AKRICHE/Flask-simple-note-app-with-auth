74eb114f43f80074969a84be7a26ea897df5d925
Thinhinane_AKRICHE <174352479+Thinhinane-AKRICHE@users.noreply.github.com>
2025-10-18 18:04:29 +0200

    login connection

--- website/auth.py
+++ website/auth.py
@@ -2,18 +2,35 @@ from flask import Blueprint, render_template, request, flash, redirect,  url_for
 from .models import User
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import db 
+from flask_login import login_user, login_required, logout_user, current_user
 
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
-    data = request.form
-    print(data)
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash('Logged in succefully!', category='success')
+                login_user(user, remember=True) #pour une deuxième connexion le serveur va se souvenir
+                return redirect(url_for('views.home'))
+            else:
+                flash('Incorrect Passeword, try again', category='error')
+        else:
+            flash('Email does not exist.', category='error')
+
     return render_template("login.html", boolean= True)
 
 @auth.route('/logout')
+@login_required
 def logout():
-    return "<p>Logout</p>"
+    logout_user()
+    return redirect(url_for('auth.login'))
+
 
 @auth.route('/sign_up', methods=['GET', 'POST'])
 def sign_up():
@@ -23,6 +40,11 @@ def sign_up():
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
 
+        '''
+        user = User.query.filter_by(email = email).first()
+        if user:
+            flash('Email already exists.', category='error')
+            '''
         if len(email) < 4:
             flash('Email must be greater than 4 charcters.', category= 'error')
         elif len(firstName) < 2:
@@ -35,6 +57,7 @@ def sign_up():
             new_user = User(email = email, first_name= firstName, password = generate_password_hash(password1, method = 'sha256'))
             db.session.add(new_user)
             db.session.commit()
+            login_user(new_user, remember=True)
             flash('Account created !', category= 'success')
             return redirect(url_for('views.home'))
             
